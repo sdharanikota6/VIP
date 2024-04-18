9852dba1f2489c07b03c1bb0b9fa8ec245c516ac
sdharanikota6 <sdharanikota6@gatech.edu>
2024-04-17 22:29:06 -0400

    updated sentiment analyses to work for 22

--- graph.py
+++ graph.py
@@ -2,14 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the data
-df = pd.read_csv('/Users/aryangarg/Documents/GitHub/VIP/dataset_with_sentiments.csv')
+df = pd.read_csv(
+    '/Users/aryangarg/Documents/GitHub/VIP/dataset_with_sentiments.csv')
 
 # Assuming 'Average_Score' column contains the sentiment scores
 # Filter out rows with no score
 df_filtered = df.dropna(subset=['Average_Score'])
 
 # Convert the 'Average_Score' to a numeric type (float)
-df_filtered['Average_Score'] = pd.to_numeric(df_filtered['Average_Score'], errors='coerce')
+df_filtered['Average_Score'] = pd.to_numeric(
+    df_filtered['Average_Score'], errors='coerce')
 
 # Drop rows where 'Average_Score' could not be converted to float
 df_filtered = df_filtered.dropna(subset=['Average_Score'])
